e59c35d8d8df05c990d0868cb6483a1528090e0b
Celian RAIMBAULT <celian.raimbault@epita.fr>
2019-10-20 13:59:40 +0200

    WIP - Added constructors

--- include/Vector.h
+++ include/Vector.h
@@ -1,1 +1,56 @@
-#define CC 123
+#pragma once
+
+#include <inttypes.h>
+
+namespace cc
+{
+	template <typename T>
+	class Vector
+	{
+	public:
+		// Data is null
+		Vector();
+		// Init the array with COUNT elements (default value)
+		Vector(const size_t COUNT);
+		// Init the array with COUNT elements of value VALUE
+		Vector(const size_t COUNT, const T VALUE);
+
+		~Vector();
+
+	private:
+		// The raw data
+		T **_data;
+
+		// Elements amount
+		size_t _count;
+	};
+
+	//// Constructors ////
+	template <typename T>
+	Vector<T>::Vector()
+		: _data(nullptr), _count(0)
+	{}
+
+	template <typename T>
+	Vector<T>::Vector(const size_t COUNT)
+		: _count(COUNT)
+	{
+		// Allocate
+		_data = new T[COUNT];
+	}
+
+	template <typename T>
+	Vector<T>::Vector(const size_t COUNT, const T VALUE)
+		: _count(COUNT)
+	{
+		// Allocate
+		_data = new T[COUNT];
+
+		// Set the data
+		for (size_t i = 0; i < COUNT; ++i)
+			data[COUNT] = VALUE;
+	}
+
+	//// Methods ////
+
+}
